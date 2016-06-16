8999ff184eadd1bdd2442034fd6824aa76fdd950
Aliaksandr Radzevich <Aliaksandr Radzevich>
2016-06-16 17:35:06 +0300

    new architecture sass and js

--- dist/js/jquery.adaptive-menu.js
+++ dist/js/jquery.adaptive-menu.js
@@ -1,7 +1,7 @@
 /*
  * jQuery adaptive menu
  * https://github.com/GREYMedia-LLC/jquery.adaptive-menu
- * version: 0.0.1
+ * version: 0.0.2
  *
  * Author: Aliaksandr Radzevich
  * Email: developer201.js@gmail.com
@@ -11,7 +11,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
 
-;(function ($) {
+;(function ($, window, document) {
 
   $.fn.adaptiveMenu = function (options) {
 
@@ -20,76 +20,82 @@
       append: 'body'
     },options||{});
 
+    var widget = this;
+
     return this.each(function () {
 
-      $(this).before('<a href="#" id="open_menu" class="adaptive-menu__open adaptive-menu__open--is-hidden"><span><b>open</b></span></a>');
-      $(this).prepend('<a href="#" id="close_menu" class="adaptive-menu__close adaptive-menu__close--is-hidden"><span>close</span></a>');
-      $(settings.append).append('<div id="adaptive_bg" class="adaptive-menu__bg"></div>');
+      function init() {
 
-      var $btn_open = $('#open_menu'),
-          $btn_close = $('#close_menu'),
-          $bg = $('#adaptive_bg'),
-          $thisMenu = $(this);
+        widget.before('<a href="#" id="open_menu" class="adaptive-menu__open adaptive-menu__open--is-hidden"><span><b>open</b></span></a>');
+        widget.prepend('<a href="#" id="close_menu" class="adaptive-menu__close adaptive-menu__close--is-hidden"><span>close</span></a>');
+        $(settings.append).append('<div id="adaptive_bg" class="adaptive-menu__bg"></div>');
+
+        var $btn_open = $('#open_menu'),
+            $btn_close = $('#close_menu'),
+            $bg = $('#adaptive_bg');
 
 
-      $(window).on('click', function (e) {
+        $(window).on('click', function (e) {
 
-        if (e.target.getAttribute('id') == 'open_menu') {
-          $btn_open.toggleClass('adaptive-menu__open--is-hidden');
-          $thisMenu.toggleClass('adaptive-menu--is-visible');
-          $btn_close.toggleClass('adaptive-menu__close--is-hidden');
-          $bg.toggleClass('adaptive-menu__bg--is-visible');
-
-          return false;
-        }
-        else if (e.target.getAttribute('id') == 'close_menu' || e.target.getAttribute('id') == 'adaptive_bg') {
-          $thisMenu.toggleClass('adaptive-menu--is-visible');
-          $btn_close.toggleClass('adaptive-menu__close--is-hidden');
-          $btn_open.toggleClass('adaptive-menu__open--is-hidden');
-          $bg.toggleClass('adaptive-menu__bg--is-visible');
-
-          return false;
-        }
-
-      });
-
-      function getWindowWidth() {
-        return window.innerWidth || document.body.clientWidth;
-      }
-
-      function queryValue() {
-        if (getWindowWidth() < settings.query) {
-         return true;
-        }
-      }
-
-      function showHideElements() {
-        if (queryValue() && !$thisMenu.hasClass('adaptive-menu')) {
-          if (!$thisMenu.hasClass('adaptive-menu--is-visible')) {
-            $thisMenu.addClass('adaptive-menu');
+          if (e.target.getAttribute('id') == 'open_menu') {
             $btn_open.toggleClass('adaptive-menu__open--is-hidden');
+            widget.toggleClass('adaptive-menu--is-visible');
+            $btn_close.toggleClass('adaptive-menu__close--is-hidden');
+            $bg.toggleClass('adaptive-menu__bg--is-visible');
+
+            return false;
           }
-          else {
-            $thisMenu.toggleClass('adaptive-menu--is-visible');
-            $thisMenu.toggleClass('adaptive-menu');
+          else if (e.target.getAttribute('id') == 'close_menu' || e.target.getAttribute('id') == 'adaptive_bg') {
+            widget.toggleClass('adaptive-menu--is-visible');
+            $btn_close.toggleClass('adaptive-menu__close--is-hidden');
             $btn_open.toggleClass('adaptive-menu__open--is-hidden');
+            $bg.toggleClass('adaptive-menu__bg--is-visible');
+
+            return false;
+          }
+
+        });
+
+        function getWindowWidth() {
+          return window.innerWidth || document.body.clientWidth;
+        }
+
+        function queryValue() {
+          if (getWindowWidth() < settings.query) {
+           return true;
           }
         }
-        else if (!queryValue() && $thisMenu.hasClass('adaptive-menu')) {
-            $thisMenu.removeClass('adaptive-menu');
-            $btn_open.addClass('adaptive-menu__open--is-hidden');
-            $btn_close.addClass('adaptive-menu__close--is-hidden');
-            $bg.removeClass('adaptive-menu__bg--is-visible');
+
+        function showHideElements() {
+          if (queryValue() && !widget.hasClass('adaptive-menu')) {
+            if (!widget.hasClass('adaptive-menu--is-visible')) {
+              widget.addClass('adaptive-menu');
+              $btn_open.toggleClass('adaptive-menu__open--is-hidden');
+            }
+            else {
+              widget.toggleClass('adaptive-menu--is-visible');
+              widget.toggleClass('adaptive-menu');
+              $btn_open.toggleClass('adaptive-menu__open--is-hidden');
+            }
+          }
+          else if (!queryValue() && widget.hasClass('adaptive-menu')) {
+              widget.removeClass('adaptive-menu');
+              $btn_open.addClass('adaptive-menu__open--is-hidden');
+              $btn_close.addClass('adaptive-menu__close--is-hidden');
+              $bg.removeClass('adaptive-menu__bg--is-visible');
+          }
         }
-      }
 
-      showHideElements();
-
-      $(window).resize(function () {
         showHideElements();
-      });
+
+        $(window).resize(function () {
+          showHideElements();
+        });
+
+      }
+      init();
 
     });
   }
 
-})(jQuery);
+})(jQuery, window, document);
--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,13 @@ var rename = require("gulp-rename");
 gulp.task('minify-css', function() {
   return gulp.src('dist/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: "-min"}))
+    .pipe(rename({suffix: ".min"}))
     .pipe(gulp.dest('dist/css'));
 });
-
-gulp.task('minify-css-t', function() {
-  return gulp.src('dist/css/themes/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(rename({suffix: "-min"}))
-    .pipe(gulp.dest('dist/css/themes'));
-});
-
 
 gulp.task('minify-js', function() {
   return gulp.src('dist/js/*.js')
     .pipe(uglify())
-    .pipe(rename({suffix: "-min"}))
+    .pipe(rename({suffix: ".min"}))
     .pipe(gulp.dest('dist/js'));
 });
